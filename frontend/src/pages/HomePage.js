c9dd07b7abf312210110f0d6f055c83b400c24b3
rafeym <mohammadrafey0@gmail.com>
2020-10-05 14:29:55 -0400

    DEVELOP: useState & useEffect hook used to retrive sample data

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,11 +1,22 @@
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
 
 // Components
 import Product from '../components/Product'
 
 const HomePage = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
+
   return (
     <>
       <h1>Latest Products</h1>
