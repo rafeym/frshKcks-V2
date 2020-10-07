ca1b654c5d51cdbf18a8d013e9e3cdd9bb7b97f7
rafeym <mohammadrafey0@gmail.com>
2020-10-07 15:46:45 -0400

    DEVELOP: addition of middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,29 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
 import connectDB from './config/db.js'
+import cors from 'cors'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
+// Routes
+import productRoutes from './routes/productRoutes.js'
+
+// Server & Database
 dotenv.config()
-
 connectDB()
-
 const app = express()
 
+// Routes
 app.get('/', (req, res) => {
-  res.send('API is running...')
+  res.send('API is running....')
 })
+app.use('/api/products', productRoutes)
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+// Middleware
+app.use(notFound)
+app.use(errorHandler)
+app.use(cors)
 
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
