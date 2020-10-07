c8a180ae5c9121201d7469592c09e14fd1ca1778
rafeym <mohammadrafey0@gmail.com>
2020-10-07 16:04:16 -0400

    REFACTOR: Removed switch import from react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 // Components
 import Header from './components/Header'
