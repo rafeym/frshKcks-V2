019c26ca99831a0d8830e9d0a1c7d8e3d3dfcf30
rafeym <mohammadrafey0@gmail.com>
2020-10-07 16:05:24 -0400

    DEVELOP: created productList and productDetail reducers

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,60 @@
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_FAIL,
+  PRODUCT_DETAILS_SUCCESS,
+} from '../actions/types'
+
+const initialState = {
+  products: [],
+}
+
+export const productListReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case PRODUCT_LIST_REQUEST:
+      return {
+        loading: true,
+        products: [],
+      }
+    case PRODUCT_LIST_SUCCESS:
+      return {
+        loading: false,
+        products: action.payload,
+      }
+    case PRODUCT_LIST_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      }
+    default:
+      return state
+  }
+}
+
+const productDetailState = {
+  product: { reviews: [] },
+}
+
+export const productDetailsReducer = (state = productDetailState, action) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return {
+        loading: true,
+        ...state,
+      }
+    case PRODUCT_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        product: action.payload,
+      }
+    case PRODUCT_DETAILS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      }
+    default:
+      return state
+  }
+}
