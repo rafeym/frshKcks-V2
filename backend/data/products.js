c9921806685f8c7c7438aa1f5f545faa61bc8ec2
rafeym <mohammadrafey0@gmail.com>
2020-10-07 14:27:21 -0400

    MISC: Dummy data to use with seeder to insert or delete data

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Yeezy Boost 350 Zebra',
+    image: '/images/yeezy-boost-350-zebra.jpg',
+    description:
+      'The popular sneaker features a Primeknit upper with a sock-like fit that sits atop full-length Boost tooling. A stripe runs along on the medial side, while a black and white-striped pattern completes the look, giving the sneaker its “Zebra” nickname.',
+    brand: 'Adidas',
+    category: 'Mens Shoes',
+    price: 1145.99,
+    inStock: 0,
+    rating: 5,
+    numReviews: 4,
+  },
+  {
+    name: 'Yeezy Boost 350 Bred',
+    image: '/images/yeezy-boost-350-bred.jpg',
+    description:
+      ' First released on February 11, 2017, the Yeezy Boost 350 V2 Bred combines a black Primeknit upper with red SPLY 350 branding, and a translucent black midsole housing full-length Boost. This colorway of Yeezy Boost 350 V2 remains one of the most coveted Yeezys in existence',
+    brand: 'Adidas',
+    category: 'Mens Shoes',
+    price: 1090.99,
+    inStock: 4,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    name: 'Air Jordan 1 Chicago Toe',
+    image: '/images/air-jordan-1-chicago-toe.jpg',
+    description:
+      'Unequivocally of that sort, this latest takes the ever-coveted, near infinitely praised “Chicago” and adds color toes the likeness of no other save for a triple black. Side panels, tongues, and even the midsoles walls go along with the classic — save for the inclusion of red tread — while accents by way of the swoosh, top line padding, aforementioned forefoot, and laces prefer the much darker shade. ',
+    brand: 'Air Jordan',
+    category: 'Mens Shoes',
+    price: 1500.99,
+    inStock: 4,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    name: 'Air Jordan 1 Light Smoke',
+    image: '/images/air-jordan-1-light-smoke.jpg',
+    description:
+      'Neutral tones of white leathers and overlays coated with the titular shade cover most of the ground, and come surrounded by that of pitch black Swooshes, tongues, and sock liners',
+    brand: 'Air Jordan',
+    category: 'Mens Shoes',
+    price: 450.99,
+    inStock: 4,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    name: 'Air Max 1 Vast-Grey',
+    image: '/images/air-max-1-vast-grey.jpg',
+    description:
+      'Crafted with cool comfort in mind, Nike’s Air Max 1 is dressed in a fresh grey and white palette. Signed off with signature a Swoosh insignia at the side wall, the mesh sneaker is overlaid with leather and suede, while Max Air cushioning is embedded securely into its foam midsole tooling.',
+    brand: 'Nike',
+    category: 'Mens Shoes',
+    price: 200.99,
+    inStock: 4,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    name: 'Yeezy Boost 350 Tail Light',
+    image: '/images/air-max-1-anniversary-red.jpg',
+    description:
+      'This Yeezy 350 V2 Tail Light is composed of a grey Primeknit upper with a translucent outer side stripe that reveals hints of orange toward the heel of the upper. A translucent Boost cushioned sole and grey interior completes the design. These sneakers released in February of 2020',
+    brand: 'Adidas',
+    category: 'Mens Shoes',
+    price: 390.99,
+    inStock: 4,
+    rating: 3.5,
+    numReviews: 4,
+  },
+]
+
+export default products
